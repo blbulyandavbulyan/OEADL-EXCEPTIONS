3d923d2d4319e93aa651bae91da932edbdfc7f94
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-04-11 00:21:33 +0400

    In module OEADL-SWING bug with wrong processing primitive types for editing in collections was fixed, but i forgot to add another files

--- src/main/java/org/blbulyandavbulyan/oeadl/exceptions/invalidtype/InvalidTypeException.java
+++ src/main/java/org/blbulyandavbulyan/oeadl/exceptions/invalidtype/InvalidTypeException.java
@@ -1,2 +1,9 @@
-package org.blbulyandavbulyan.oeadl.exceptions.invalidtype;public class InvalidTypeException {
+package org.blbulyandavbulyan.oeadl.exceptions.invalidtype;
+
+import org.blbulyandavbulyan.oeadl.exceptions.OEADLException;
+
+public abstract class InvalidTypeException extends OEADLException {
+    public InvalidTypeException(String msg) {
+        super(msg);
+    }
 }
--- src/main/java/org/blbulyandavbulyan/oeadl/exceptions/invalidtype/UnsupportedTypeException.java
+++ src/main/java/org/blbulyandavbulyan/oeadl/exceptions/invalidtype/UnsupportedTypeException.java
@@ -1,2 +1,12 @@
-package org.blbulyandavbulyan.oeadl.exceptions.invalidtype;public class UnsupportedTypeException {
+package org.blbulyandavbulyan.oeadl.exceptions.invalidtype;
+
+public class UnsupportedTypeException extends InvalidTypeException{
+    private final Class<?> causeType;
+    public UnsupportedTypeException(Class<?> causeType){
+        super(String.format("Type(class) %s in the inner map not found!", causeType.getName()));
+        this.causeType = causeType;
+    }
+    public Class<?> getCauseType(){
+        return causeType;
+    }
 }
